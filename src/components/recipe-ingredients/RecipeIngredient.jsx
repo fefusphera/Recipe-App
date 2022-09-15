436ccf4322c77422b6312ed0d9b56109b70462d8
fefusphera <fefu.sphera@hotmail.com>
2022-09-15 17:46:19 +0200

    fixed Ingredients

--- src/components/recipe-ingredients/RecipeIngredient.jsx
+++ src/components/recipe-ingredients/RecipeIngredient.jsx
@@ -4,14 +4,29 @@ import { Fragment } from "react";
 
 const RecipeIngredient = () => {
   const recipe = useOutletContext();
+
   const indexes = Array.from({ length: 20 }, (_, i) => i + 1);
 
   // console.log(recipe.meals[0]);
   // console.log(indexes);
+  console.log(`recipe.meals[0].strIngredient${indexes}`);
 
   return (
     <div>
       <h1>Ingredienti:</h1>
+      <ul>
+        {!!recipe &&
+          indexes.map((index) => (
+            <Fragment key={index}>
+              {recipe.meals[0][`strIngredient${index}`]?.length ? (
+                <li>
+                  {recipe.meals[0][`strIngredient${index}`]}:
+                  {recipe.meals[0][`strMeasure${index}`]}
+                </li>
+              ) : null}
+            </Fragment>
+          ))}
+      </ul>
     </div>
   );
 };
